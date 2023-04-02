566a2447712d99d41e85d8e4dd902acf1a11c26e
folkmanis <folkmanis@gmail.com>
2023-04-02 22:06:42 +0300

    Minute bugfix

--- app/src/main/java/com/folkmanis/laiks/utilities/ClockTicks.kt
+++ app/src/main/java/com/folkmanis/laiks/utilities/ClockTicks.kt
@@ -2,14 +2,13 @@ package com.folkmanis.laiks.utilities
 
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flow
-import java.time.Instant
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
 
 fun minuteTicks() = flow<LocalDateTime> {
     while (true) {
         val now = LocalDateTime.now()
-        emit(now)
+        emit(now.truncatedTo(ChronoUnit.MINUTES))
         val delayToNext = delayToNextMinute(LocalDateTime.now())
         delay(delayToNext)
     }
