3930a2c5b1b9a751ef3b31bd94eeac7469287a52
folkmanis <folkmanis@gmail.com>
2023-04-19 14:05:29 +0300

    Animated

--- app/src/main/java/com/folkmanis/laiks/utilities/composables/LoadingScreen.kt
+++ app/src/main/java/com/folkmanis/laiks/utilities/composables/LoadingScreen.kt
@@ -1,28 +1,86 @@
 package com.folkmanis.laiks.utilities.composables
 
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.folkmanis.laiks.R
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier.fillMaxSize()
     ) {
-        Image(
-            modifier = Modifier.size(200.dp),
-            painter = painterResource(R.drawable.loading_img),
-            contentDescription = stringResource(R.string.loading)
+        LoadingAnimation(
+            circleColor = Color.DarkGray,
+            modifier = Modifier.size(size = 128.dp)
         )
     }
 }
 
+@Composable
+fun LoadingAnimation(
+    modifier: Modifier = Modifier,
+    circleColor: Color = Color.Magenta,
+    animationDelay: Int = 1000,
+) {
+
+    // circle's scale state
+    var circleScale by remember {
+        mutableStateOf(0f)
+    }
+
+    // animation
+    val circleScaleAnimate = animateFloatAsState(
+        targetValue = circleScale,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = animationDelay
+            )
+        )
+    )
+
+    // This is called when the app is launched
+    LaunchedEffect(Unit) {
+        circleScale = 1f
+    }
+
+    // animating circle
+    Box(
+        modifier = modifier
+            .scale(scale = circleScaleAnimate.value)
+            .border(
+                width = 4.dp,
+                color = circleColor.copy(alpha = 1 - circleScaleAnimate.value),
+                shape = CircleShape
+            )
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+fun LoadingScreenPreview() {
+    MaterialTheme {
+        LoadingScreen()
+    }
+}
