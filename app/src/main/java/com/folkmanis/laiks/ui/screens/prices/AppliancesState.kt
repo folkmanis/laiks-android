287a3031bb679fa63c3189ef3d02a29958e9a579
folkmanis <folkmanis@gmail.com>
2023-04-24 11:13:02 +0300

    cleanup

--- app/src/main/java/com/folkmanis/laiks/ui/screens/prices/AppliancesState.kt
+++
@@ -1,7 +0,0 @@
-package com.folkmanis.laiks.ui.screens.prices
-
-import com.folkmanis.laiks.model.PowerApplianceHour
-
-data class AppliancesState(
-    val powerAppliancesHours: Map<Int, List<PowerApplianceHour>> = emptyMap()
-)
