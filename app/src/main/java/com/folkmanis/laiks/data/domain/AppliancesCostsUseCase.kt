a89c9c7cecd7f1fef1f9adce1433a3ee31ea7e24
folkmanis <folkmanis@gmail.com>
2023-04-24 11:13:28 +0300

    minute costs moved from viewModel

---
+++ app/src/main/java/com/folkmanis/laiks/data/domain/AppliancesCostsUseCase.kt
@@ -0,0 +1,84 @@
+package com.folkmanis.laiks.data.domain
+
+import com.folkmanis.laiks.data.AppliancesService
+import com.folkmanis.laiks.model.NpPrice
+import com.folkmanis.laiks.model.PowerAppliance
+import com.folkmanis.laiks.model.PowerApplianceHour
+import com.folkmanis.laiks.utilities.bestOffset
+import com.folkmanis.laiks.utilities.ext.hoursFrom
+import com.folkmanis.laiks.utilities.offsetCosts
+import java.time.LocalDateTime
+import java.time.ZoneId
+import javax.inject.Inject
+
+private fun List<PowerAppliance>.toPowerApplianceHour(
+    appliancesAllCosts: Map<PowerAppliance, Map<Long, Double>>,
+    offset: Int,
+    bestOffsets: Map<PowerAppliance, Int>
+): List<PowerApplianceHour> {
+    val appliances = this
+    return buildList {
+        appliances.forEach { appliance ->
+            val applianceHourCost = appliancesAllCosts[appliance]
+                ?.get(offset.toLong())
+            if (applianceHourCost != null) {
+                add(
+                    PowerApplianceHour(
+                        name = appliance.name,
+                        cost = applianceHourCost,
+                        color = appliance.color,
+                        isBest = bestOffsets[appliance] == offset,
+                    )
+                )
+            }
+        }
+    }
+}
+
+class AppliancesCostsUseCase @Inject constructor(
+    private val appliancesService: AppliancesService,
+) {
+
+    suspend operator fun invoke(
+        prices: List<NpPrice>,
+        minute: LocalDateTime,
+    ): Map<Int, List<PowerApplianceHour>> {
+
+        val appliances = appliancesService.activeAppliances()
+
+        val startTime = minute.atZone(ZoneId.systemDefault()).toInstant()
+        val appliancesAllCosts: Map<PowerAppliance, Map<Long, Double>> =
+            buildMap {
+                appliances.forEach { appliance ->
+                    val costs = offsetCosts(prices, startTime, appliance)
+                    put(appliance, costs)
+                }
+            }
+
+        val bestOffsets = buildMap {
+            appliancesAllCosts.forEach { (powerAppliance, costs) ->
+                val bestOffset = costs
+                    .bestOffset()
+                if (bestOffset != null) {
+                    put(powerAppliance, bestOffset)
+                }
+            }
+        }
+
+        val hourlyCosts: Map<Int, List<PowerApplianceHour>> = buildMap {
+            prices.forEach { npPrice ->
+                val offset = npPrice.startTime.hoursFrom(minute)
+                val appliancesHour = appliances.toPowerApplianceHour(
+                    appliancesAllCosts,
+                    offset,
+                    bestOffsets
+                )
+                put(offset, appliancesHour)
+            }
+        }
+
+        return hourlyCosts
+
+    }
+
+}
--- app/src/main/java/com/folkmanis/laiks/ui/screens/prices/PricesViewModel.kt
+++ app/src/main/java/com/folkmanis/laiks/ui/screens/prices/PricesViewModel.kt
@@ -3,26 +3,21 @@ package com.folkmanis.laiks.ui.screens.prices
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.folkmanis.laiks.INCLUDE_AVERAGE_DAYS
-import com.folkmanis.laiks.data.AppliancesService
+import com.folkmanis.laiks.data.domain.AppliancesCostsUseCase
 import com.folkmanis.laiks.data.domain.LastDaysPricesUseCase
 import com.folkmanis.laiks.model.NpPrice
-import com.folkmanis.laiks.model.PowerAppliance
 import com.folkmanis.laiks.model.PowerApplianceHour
-import com.folkmanis.laiks.utilities.bestOffset
 import com.folkmanis.laiks.utilities.ext.*
 import com.folkmanis.laiks.utilities.hourTicks
 import com.folkmanis.laiks.utilities.minuteTicks
-import com.folkmanis.laiks.utilities.offsetCosts
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
-import java.time.LocalDateTime
-import java.time.ZoneId
 import javax.inject.Inject
 
 @HiltViewModel
 class PricesViewModel @Inject constructor(
-    appliancesService: AppliancesService,
     lastDaysPrices: LastDaysPricesUseCase,
+    private val appliancesCosts: AppliancesCostsUseCase,
 ) : ViewModel() {
 
     private val lastPricesFlow: Flow<List<NpPrice>> =
@@ -52,78 +47,9 @@ class PricesViewModel @Inject constructor(
 
     val appliancesState: Flow<Map<Int, List<PowerApplianceHour>>> =
         combine(minuteTicks(), lastPricesFlow) { minute, prices ->
-            val appliances = appliancesService.activeAppliances()
-            appliancesCostsFromMinute(
-                prices,
-                minute,
-                appliances
-            )
+            appliancesCosts(prices, minute)
         }
 
-    private fun appliancesCostsFromMinute(
-        prices: List<NpPrice>,
-        minute: LocalDateTime,
-        appliances: List<PowerAppliance>,
-    ): Map<Int, List<PowerApplianceHour>> {
-
-        val startTime = minute.atZone(ZoneId.systemDefault()).toInstant()
-        val appliancesAllCosts: Map<PowerAppliance, Map<Long, Double>> =
-            buildMap {
-                appliances.forEach { appliance ->
-                    val costs = offsetCosts(prices, startTime, appliance)
-                    put(appliance, costs)
-                }
-            }
-
-        val bestOffsets = buildMap {
-            appliancesAllCosts.forEach { (powerAppliance, costs) ->
-                val bestOffset = costs
-                    .bestOffset()
-                if (bestOffset != null) {
-                    put(powerAppliance, bestOffset)
-                }
-            }
-        }
-
-        val hourlyCosts: Map<Int, List<PowerApplianceHour>> = buildMap {
-            prices.forEach { npPrice ->
-                val offset = npPrice.startTime.hoursFrom(minute)
-                val appliancesHour = appliances.toPowerApplianceHour(
-                    appliancesAllCosts,
-                    offset,
-                    bestOffsets
-                )
-                put(offset, appliancesHour)
-            }
-        }
-
-        return hourlyCosts
-
-    }
-
-    private fun List<PowerAppliance>.toPowerApplianceHour(
-        appliancesAllCosts: Map<PowerAppliance, Map<Long, Double>>,
-        offset: Int,
-        bestOffsets: Map<PowerAppliance, Int>
-    ): List<PowerApplianceHour> {
-        val appliances = this
-        return buildList {
-            appliances.forEach { appliance ->
-                val applianceHourCost = appliancesAllCosts[appliance]
-                    ?.get(offset.toLong())
-                if (applianceHourCost != null) {
-                    add(
-                        PowerApplianceHour(
-                            name = appliance.name,
-                            cost = applianceHourCost,
-                            color = appliance.color,
-                            isBest = bestOffsets[appliance] == offset,
-                        )
-                    )
-                }
-            }
-        }
-    }
 
     companion object {
         @Suppress("unused")
