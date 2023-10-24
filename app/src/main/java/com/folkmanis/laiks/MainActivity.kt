fd3e809d5ef69f017721ecfe92a41f3e4c3e72b0
folkmanis <folkmanis@gmail.com>
2023-10-24 13:36:24 +0300

    added admob properties

--- app/src/main/java/com/folkmanis/laiks/MainActivity.kt
+++ app/src/main/java/com/folkmanis/laiks/MainActivity.kt
@@ -3,11 +3,18 @@ package com.folkmanis.laiks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.ui.Modifier
 import com.folkmanis.laiks.ui.screens.laiks.LaiksAppScreen
 import com.folkmanis.laiks.ui.theme.LaiksTheme
+import com.folkmanis.laiks.utilities.composables.AdmobBanner
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -16,14 +23,22 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) {}
 
         setContent {
             LaiksTheme {
                 val windowSize = calculateWindowSizeClass(activity = this)
                 Surface {
-                    LaiksAppScreen(
-                        windowSize=windowSize
-                    )
+                    Column(
+                            modifier = Modifier.fillMaxSize()
+                    ) {
+
+                        LaiksAppScreen(
+                                windowSize = windowSize,
+                                modifier = Modifier.fillMaxHeight()
+                        )
+                        AdmobBanner()
+                    }
                 }
             }
         }
