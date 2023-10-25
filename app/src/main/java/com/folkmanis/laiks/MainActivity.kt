f63c233a60f557b5b677c37723fa2db2c9bf5cbc
folkmanis <folkmanis@gmail.com>
2023-10-25 10:41:47 +0300

    enabled adMob

--- app/src/main/java/com/folkmanis/laiks/MainActivity.kt
+++ app/src/main/java/com/folkmanis/laiks/MainActivity.kt
@@ -3,18 +3,18 @@ package com.folkmanis.laiks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.folkmanis.laiks.ui.screens.laiks.LaiksAppScreen
 import com.folkmanis.laiks.ui.theme.LaiksTheme
 import com.folkmanis.laiks.utilities.composables.AdmobBanner
 import com.google.android.gms.ads.MobileAds
-import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.AndroidEntryPoint
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -29,15 +29,19 @@ class MainActivity : ComponentActivity() {
             LaiksTheme {
                 val windowSize = calculateWindowSizeClass(activity = this)
                 Surface {
-                    Column(
-                            modifier = Modifier.fillMaxSize()
-                    ) {
+
+                    Box {
 
                         LaiksAppScreen(
-                                windowSize = windowSize,
-                                modifier = Modifier.fillMaxHeight()
+                            windowSize = windowSize,
+                            modifier = Modifier
+                                .padding(bottom = 50.dp)
                         )
-                        AdmobBanner()
+
+                        AdmobBanner(
+                            modifier = Modifier
+                                .align(Alignment.BottomCenter)
+                        )
                     }
                 }
             }
--- app/src/main/java/com/folkmanis/laiks/utilities/composables/AdmobBaner.kt
+++ app/src/main/java/com/folkmanis/laiks/utilities/composables/AdmobBaner.kt
@@ -3,20 +3,23 @@ package com.folkmanis.laiks.utilities.composables
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
+import com.folkmanis.laiks.BuildConfig
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 
 @Composable
 fun AdmobBanner(
-        modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier,
 ) {
     AndroidView(
-            factory ={context->
-                         AdView(context).apply {
-                             setAdSize(AdSize.BANNER)
-                             adUnitId = "ca-app-pub-3940256099942544/6300978111"
-                             loadAd(AdRequest.Builder().build())
-                         }
-    }, modifier=modifier)
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                adUnitId = BuildConfig.AD_UNIT_ID
+                loadAd(AdRequest.Builder().build())
+            }
+        },
+        modifier = modifier
+    )
 }
