7b25763a9b2a14480d5f74454fcc2e276e993336
folkmanis <folkmanis@gmail.com>
2023-10-28 21:48:33 +0300

    added email auth

--- app/src/main/java/com/folkmanis/laiks/utilities/oauth/GoogleOauth.kt
+++ app/src/main/java/com/folkmanis/laiks/utilities/oauth/GoogleOauth.kt
@@ -2,15 +2,18 @@ package com.folkmanis.laiks.utilities.oauth
 
 import android.content.Intent
 import com.firebase.ui.auth.AuthUI
+import com.folkmanis.laiks.R
 
 fun getSignInIntent(): Intent {
     val providers = arrayListOf(
-        AuthUI.IdpConfig.GoogleBuilder().build()
+        AuthUI.IdpConfig.GoogleBuilder().build(),
+        AuthUI.IdpConfig.EmailBuilder().build(),
     )
 
     return AuthUI.getInstance()
         .createSignInIntentBuilder()
         .setAvailableProviders(providers)
+        .setLogo(R.drawable.clock_clock)
         .build()
 
 }
