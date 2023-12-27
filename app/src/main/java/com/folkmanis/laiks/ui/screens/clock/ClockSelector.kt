d746ab711555426cb61c87ed6db986a7eb31cf50
folkmanis <folkmanis@gmail.com>
2023-12-27 08:27:43 +0200

    Layout created

--- app/src/main/java/com/folkmanis/laiks/ui/screens/clock/ClockSelector.kt
+++ app/src/main/java/com/folkmanis/laiks/ui/screens/clock/ClockSelector.kt
@@ -1,259 +1,221 @@
 package com.folkmanis.laiks.ui.screens.clock
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material3.Icon
-import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.folkmanis.laiks.R
-import com.folkmanis.laiks.STEP_DOWN_VALUE
-import com.folkmanis.laiks.STEP_UP_VALUE
+import com.folkmanis.laiks.data.fake.FakeLaiksUserService
 import com.folkmanis.laiks.model.UserPowerAppliance
-import com.folkmanis.laiks.ui.theme.LaiksTheme
 import com.folkmanis.laiks.utilities.ext.hoursString
 import com.folkmanis.laiks.utilities.ext.minutesString
-import com.folkmanis.laiks.utilities.ext.toSignedString
 import java.time.LocalTime
 
 @Composable
-fun ClockSelector(
+fun VerticalClockSelector(
     offset: Int,
     time: LocalTime,
     onOffsetChange: (Int) -> Unit,
     appliances: List<UserPowerAppliance>,
     onSelected: (idx: Int, name: String) -> Unit,
-    isNarrow: Boolean,
     modifier: Modifier = Modifier,
 ) {
-    if (isNarrow) {
+
+    VerticalClockLayout(
+        modifier = modifier,
+    ) {
+
+        AppliancesSelector(
+            appliances = appliances,
+            onSelected = onSelected,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+//                .weight(1f)
+        )
+
+        VerticalUpDownButtons(
+            offset = offset,
+            onOffsetChange = onOffsetChange,
+            modifier = Modifier.layoutId("normal")
+        )
+
+        LargeVerticalUpDownButtons(
+            offset = offset,
+            onOffsetChange = onOffsetChange,
+            modifier = Modifier.layoutId("large")
+        )
+
+        TimeComponent(
+            hours = time.hoursString,
+            minutes = time.minutesString,
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+//                .weight(1f),
+        )
+
+    }
+}
+
+@Composable
+fun VerticalClockLayout(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        content = content,
+        modifier = modifier
+    ) { measurables, constraints ->
+        val constrainedHeight=constraints.copy(
+            minHeight = 0
+        )
+        Log.d(TAG, "maxHeight: ${constraints.maxHeight}")
+        val topPlaceables = measurables
+            .takeWhile { it.layoutId == null }
+            .map { it.measure(constrainedHeight) }
+        val bottomPlaceables = measurables
+            .takeLastWhile { it.layoutId == null }
+            .map { it.measure(constrainedHeight) }
+        val normalPlaceable = measurables
+            .find { it.layoutId == "normal" }
+            ?.measure(constrainedHeight)
+        val largePlaceable = measurables
+            .find { it.layoutId == "large" }
+            ?.measure(constrainedHeight)
+        val largeHeight = topPlaceables.sumOf { it.height } +
+                bottomPlaceables.sumOf { it.height } +
+                (largePlaceable?.height ?: 0)
+        val normalHeight = topPlaceables.sumOf { it.height } +
+                bottomPlaceables.sumOf { it.height } +
+                (normalPlaceable?.height ?: 0)
+        val isLarge = largeHeight >= constraints.maxHeight
+        Log.d(TAG, "largeHeight: ${largeHeight}")
+        Log.d(TAG, "normalHeight: ${normalHeight}")
+
+        val width = if (isLarge) largeHeight else normalHeight
+        val height = constraints.maxHeight
+
+        layout(width, height) {
+            var yPos = 0
+            val center = width / 2
+            topPlaceables.forEach { placeable ->
+                placeable.placeRelative(
+                    center - placeable.width / 2,
+                    yPos
+                )
+                yPos += placeable.height
+            }
+
+            if (isLarge) {
+                largePlaceable?.also { placeable ->
+                    placeable.placeRelative(
+                        center - placeable.width / 2,
+                        yPos
+                    )
+                    yPos += placeable.height
+                }
+            } else {
+                normalPlaceable?.also { placeable ->
+                    placeable.placeRelative(
+                        center - placeable.width / 2,
+                        yPos
+                    )
+                    yPos += placeable.height
+                }
+            }
+
+            bottomPlaceables.forEach { placeable ->
+                placeable.placeRelative(
+                    center - placeable.width / 2,
+                    yPos
+                )
+                yPos += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun HorizontalClockSelector(
+    offset: Int,
+    time: LocalTime,
+    onOffsetChange: (Int) -> Unit,
+    appliances: List<UserPowerAppliance>,
+    onSelected: (idx: Int, name: String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+
         Column(
-            modifier = modifier,
+            modifier = Modifier.weight(1f),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-
-            AppliancesSelector(
-                appliances = appliances,
-                onSelected = onSelected,
-                modifier = Modifier
-                    .padding(bottom = 16.dp)
-                    .weight(1f),
-            )
-
-            UpDownButtons(
-                offset = offset,
-                onOffsetChange = onOffsetChange,
-            )
 
             TimeComponent(
                 hours = time.hoursString,
                 minutes = time.minutesString,
                 modifier = Modifier
-                    .padding(vertical = 16.dp)
-                    .weight(1f),
+                    .padding(horizontal = 16.dp)
             )
 
-        }
-    } else {
-        Row(
-            modifier = modifier,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+            Spacer(modifier = Modifier.height(16.dp))
 
-            Column(
-                modifier = Modifier.weight(1f),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-
-                TimeComponent(
-                    hours = time.hoursString,
-                    minutes = time.minutesString,
-                    modifier = Modifier
-                        .padding(horizontal = 16.dp)
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                UpDownButtons(
-                    offset = offset,
-                    onOffsetChange = onOffsetChange,
-                    horizontalLayout = true,
-                )
-
-            }
-
-            AppliancesSelector(
-                appliances = appliances,
-                onSelected = onSelected,
-                modifier = Modifier.weight(0.67f)
-            )
-
-        }
-    }
-
-}
-
-@Composable
-fun UpDownButtons(
-    offset: Int,
-    onOffsetChange: (Int) -> Unit,
-    modifier: Modifier = Modifier,
-    horizontalLayout: Boolean = false,
-) {
-
-    if (horizontalLayout) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier,
-        ) {
-
-            OffsetChangeButton(
-                onClick = { onOffsetChange(STEP_DOWN_VALUE) },
-                iconId = R.drawable.remove,
-                descriptionId = R.string.hour_minus_button,
-            )
-
-            OffsetHours(
-                offset,
-                modifier = Modifier
-                    .padding(horizontal = 8.dp)
-                    .wrapContentWidth()
-            )
-
-            OffsetChangeButton(
-                onClick = { onOffsetChange(STEP_UP_VALUE) },
-                iconId = R.drawable.add,
-                descriptionId = R.string.hour_plus_button,
-            )
-
-        }
-    } else {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = modifier,
-        ) {
-
-            OffsetChangeButton(
-                onClick = { onOffsetChange(STEP_UP_VALUE) },
-                iconId = R.drawable.add,
-                descriptionId = R.string.hour_plus_button,
-            )
-
-            OffsetHours(
-                offset,
-                modifier = Modifier.padding(vertical = 8.dp)
-            )
-
-            OffsetChangeButton(
-                onClick = { onOffsetChange(STEP_DOWN_VALUE) },
-                iconId = R.drawable.remove,
-                descriptionId = R.string.hour_minus_button,
+            HorizontalUpDownButtons(
+                offset = offset,
+                onOffsetChange = onOffsetChange,
             )
 
         }
 
-    }
-
-}
-
-@Composable
-fun OffsetHours(
-    offset: Int,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = offset.toSignedString(),
-        fontSize = 64.sp,
-        fontWeight = FontWeight.ExtraBold,
-        modifier = modifier
-    )
-}
-
-@Composable
-fun OffsetChangeButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    @DrawableRes iconId: Int,
-    @StringRes descriptionId: Int,
-) {
-    LargeFloatingActionButton(
-        onClick = onClick,
-        modifier = modifier
-    ) {
-        Icon(
-            painter = painterResource(iconId),
-            contentDescription = stringResource(descriptionId),
-        )
-    }
-}
-
-@Composable
-fun TimeComponent(
-    hours: String,
-    minutes: String,
-    modifier: Modifier = Modifier
-) {
-
-    Row(modifier = modifier) {
-
-        TimeSymbols(text = hours)
-
-        TimeSymbols(
-            text = stringResource(id = R.string.minutes_separator),
+        AppliancesSelector(
+            appliances = appliances,
+            onSelected = onSelected,
+            modifier = Modifier.weight(0.67f)
         )
 
-        TimeSymbols(text = minutes)
-
     }
 }
 
+@Preview(device = "spec:width=392.7dp,height=400dp,dpi=320")
 @Composable
-fun TimeSymbols(
-    text: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = text,
-        fontWeight = FontWeight.Bold,
-        fontSize = 48.sp,
-        modifier = modifier,
-    )
-}
-
-
-@Preview
-@Composable
-fun UpDownButtonsPreviewVertical() {
-    LaiksTheme {
-        UpDownButtons(
+fun VerticalClockSelectorPreview() {
+    MaterialTheme {
+        VerticalClockSelector(
             offset = 2,
-            onOffsetChange = {}
-        )
-    }
-}
-
-@Preview
-@Composable
-fun UpDownButtonsPreviewHorizontal() {
-    LaiksTheme {
-        UpDownButtons(
-            offset = 2,
+            time = LocalTime.now(),
             onOffsetChange = {},
-            horizontalLayout = true
+            appliances = FakeLaiksUserService.laiksUser.appliances,
+            onSelected = { _, _ -> },
+        )
+    }
+}
+
+@Preview(device = "spec:width=411dp,height=891dp")
+@Composable
+fun LargeVerticalClockSelectorPreview() {
+    MaterialTheme {
+        VerticalClockSelector(
+            offset = 2,
+            time = LocalTime.now(),
+            onOffsetChange = {},
+            appliances = FakeLaiksUserService.laiksUser.appliances,
+            onSelected = { _, _ -> },
+            modifier = Modifier.fillMaxHeight()
         )
     }
 }
