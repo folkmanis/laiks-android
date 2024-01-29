24e0ddc8a796aaea21bf6a4994b7089227114982
folkmanis <folkmanis@gmail.com>
2024-01-29 08:12:43 +0200

    splash-screen

--- app/src/main/java/com/folkmanis/laiks/MainActivity.kt
+++ app/src/main/java/com/folkmanis/laiks/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.folkmanis.laiks.ui.screens.laiks.LaiksAppNavigation
 import com.folkmanis.laiks.ui.theme.LaiksTheme
 import com.folkmanis.laiks.utilities.composables.AdmobBanner
@@ -27,6 +28,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+        
         MobileAds.initialize(this) {}
 
         setContent {
